d418503c1940d0ab47cb5c021ff51ae7333364fb
Harsh Kumar <kunarharsh76@gmail.com>
2025-09-05 12:44:38 +0530

    Add files via upload

---
+++ EXP14-c.cpp
@@ -0,0 +1,37 @@
+//Harsh kumar
+//24070123146   
+//B3
+//Multilevel inheritance
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Food {
+    public:
+    string cuisine="Indian";
+    void type(){
+        cout<<"Indian Subcontinental"<<endl;
+    }
+};
+class Dish: public Food {
+    public:
+    string dish="Chole Bhature";
+};
+class Restaurant: public Dish {
+    public:
+    string name="Harsh's Kitchen";
+};
+
+int main(){
+    Restaurant f3;
+    f3.type();
+    cout<<f3.cuisine<<": "<<f3.dish<<endl;
+    cout<<"Restaurant: "<<f3.name;
+}
+
+/* OUTPUT:
+Indian Subcontinental
+Indian: Chole Bhature
+Restaurant: Harsh's Kitchen
+*/
